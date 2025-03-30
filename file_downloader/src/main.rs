cb5079ddf41d5f431a5f172a28e055bae1f059c3
Nikola Jolovic <nikolajolovic02@gmail.com>
2025-03-30 23:29:49 +0200

    Add downloader call to main function

--- file_downloader/src/main.rs
+++ file_downloader/src/main.rs
@@ -1,9 +1,28 @@
 use crate::config::Config;
+use crate::downloader::Downloader;
+use sha2::{Digest, Sha256};
+use std::error::Error;
+use std::fs;
 
 mod config;
+mod downloader;
+mod http;
 
-fn main() {
-    let config = Config::from(std::env::args().collect());
+fn main() -> Result<(), Box<dyn Error>> {
+    let config = Config::from(std::env::args().collect::<Vec<String>>());
 
+    let downloader = Downloader::new(config.server_url);
 
+    let data = downloader.download_fully()?;
+
+    let hash = Sha256::digest(&data);
+    println!("SHA-256 hash of received data: {:x}", hash);
+
+    fs::write(&config.output_filename, data)?;
+    println!(
+        "Binary contents successfully written to {}",
+        config.output_filename
+    );
+
+    Ok(())
 }
